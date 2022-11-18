260fba51b28ec74230b6ce3e8fc46f7dcc3eefb5
Lachlan Fry <lachlan.fry@dps.com.au>
2022-11-18 14:09:16 +1100

    updates

--- start_atlas.sh
+++ start_atlas.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ADMIN_PASS="123"
+PUBLIC_IP="10.0.0.49"
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-SETTINGS_STRING="Ocean?ServerAdminPassword=$ADMIN_PASS?ServerX=0?ServerY=0?MaxPlayers=10?ReservedPlayerSlots=5?QueryPort=57555?Port=5755?RCONEnabled=true?RCONPort=28000?SeamlessIP=10.0.0.49 -NoBattlEye -log -server"
+SETTINGS_STRING="Ocean?ServerAdminPassword=$ADMIN_PASS?ServerX=0?ServerY=0?MaxPlayers=10?ReservedPlayerSlots=5?QueryPort=57555?Port=5755?RCONEnabled=true?RCONPort=28000?SeamlessIP=$PUBLIC_IP -NoBattlEye -log -server"
   
 docker run -d -t --name=atlas-server \
 --net=host \
